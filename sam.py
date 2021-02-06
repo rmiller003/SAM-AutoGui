aee539b06bb388f7e187891dedf95c72bcad5d67
rmill <rmiller003@gmail.com>
2021-02-06 16:31:53 -0500

    Ver 1

---
+++ sam.py
@@ -0,0 +1,21 @@
+# This is my S.A.M Robotic Arm program using the Pyautogui module
+# Designed by Robert Miller in Python 3.8.5
+
+import pyautogui
+import time
+
+pyautogui.click(755, 1185)
+time.sleep(2)
+pyautogui.click(749, 920)
+time.sleep(3)
+pyautogui.click(912, 552)
+time.sleep(3)
+pyautogui.click(1369, 907)
+time.sleep(3)
+pyautogui.click(1209, 920)
+time.sleep(3)
+pyautogui.click(1241, 920)
+time.sleep(45)
+pyautogui.click(1666, 191)
+
+
